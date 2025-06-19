d4d027c0870d61e1fa93730909c01debe6c290e7
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2025-06-19 21:06:17 +0500

    Create Chat Container Header Component

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import ChatContainer from "../components/ChatContainer"
 import Sidebar from "../components/Sidebar"
 
 type User = {
@@ -13,6 +14,7 @@ const Home = () => {
     <div className="border w-full h-screen sm:px-[15%] sm:py-[5%]">
       <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]': 'md:grid-cols-2'}`}>
         <Sidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
+        <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
       </div>
     </div>
   )
