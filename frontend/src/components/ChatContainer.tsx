56a9a35cd72c5099cd8f2183d887ea6f292039c1
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2025-06-20 20:57:49 +0500

    Integrate the Dummy Data in Chat Container

--- frontend/src/components/ChatContainer.tsx
+++ frontend/src/components/ChatContainer.tsx
@@ -1,5 +1,5 @@
 import type React from "react";
-import assets from "../assets/assets";
+import assets, { messagesDummyData } from "../assets/assets";
 
 type User = {
   _id: string;
@@ -16,7 +16,7 @@ const ChatContainer: React.FC<ChatContainerProps> = ({
   selectedUser,
   setSelectedUser,
 }) => {
-  return selectedUser !==null ? (
+  return selectedUser !== null ? (
     <div className="h-full overflow-scroll relative backdrop-blur-lg">
       <div className="flex items-center gap-3 py-3 mx-4 border-b border-stone-500">
         <img src={assets.profile_martin} alt="" className="w-8 rounded-full" />
@@ -33,6 +33,46 @@ const ChatContainer: React.FC<ChatContainerProps> = ({
           }}
         />
         <img src={assets.help_icon} alt="" className="max-md:hidden max-w-5" />
+      </div>
+      <div className="flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6">
+        {messagesDummyData.map((msg, index) => (
+          <div
+            key={index}
+            className={`flex items-end justify-around ${
+              msg?.senderId !== "680f50e4f10f3cd28382ecf9" && "flex-row-reverse"
+            }`}
+          >
+            {msg.image ? (
+              <img
+                src={msg.image}
+                alt=""
+                className="max-w-[230px] border border-gray-700 rounded-lg overflow-hidden mb-8"
+              />
+            ) : (
+              <p
+                className={`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white ${
+                  msg.senderId === "680f50e4f10f3cd28382ecf9"
+                    ? "rounded-br-none"
+                    : "rounded-bl-none"
+                }`}
+              >
+                {msg.text}
+              </p>
+            )}
+            <div className="text-center text-xs">
+              <img
+                src={
+                  msg.senderId === "680f50e4f10f3cd28382ecf9"
+                    ? msg.image
+                    : assets.avatar_icon
+                }
+                alt=""
+                className="w-7 rounded-full"
+              />
+              <p className="text-gray-500">{msg.createdAt}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   ) : (
