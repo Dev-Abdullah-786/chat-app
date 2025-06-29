8a7d620eb4a6fccaf902c2b94942ea6446afb68d
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2025-06-29 10:46:53 +0500

    Create Error Handler Middleware

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ dotenv.config({ path: "./config/.env" });
 const { createServer } = require("http");
 const cors = require("cors");
 const connectDb = require("./database/connectDb");
+const { errorMiddleware } = require("./middlewares/error.Middleware");
 
 const httpServer = createServer(app);
 
@@ -12,5 +13,7 @@ app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
 connectDb()
+
+app.use(errorMiddleware)
 
 module.exports = httpServer;
