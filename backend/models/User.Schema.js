540ed9e86d9bda1f560323f37810d3ecab61ad20
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2025-07-01 21:14:38 +0500

    add password hashing, comparison, and JWT token generation methods

--- backend/models/User.Schema.js
+++ backend/models/User.Schema.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema(
   {
@@ -30,6 +32,30 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) {
+    return next();
+  }
+
+  const salt = bcrypt.genSaltSync(10);
+  const hashPassword = bcrypt.hashSync(this.password, salt);
+  this.password = hashPassword;
+});
+
+// Compare Password
+userSchema.methods.checkPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// Generate JWT Token
+userSchema.methods.generateToken = async function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
