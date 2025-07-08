6cbec0de159a37082769a9415fd85f2a43f2b57c
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2025-07-08 20:53:34 +0500

    integrate Socket.IO for real-time user connection tracking

--- backend/app.js
+++ backend/app.js
@@ -4,19 +4,44 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config/.env" });
 const { createServer } = require("http");
 const cors = require("cors");
+const { Server } = require("socket.io");
 const connectDb = require("./database/connectDb");
 const { errorMiddleware } = require("./middlewares/error.Middleware");
 const { userRouter } = require("./routes/User.Routes");
 
 const httpServer = createServer(app);
 
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+module.exports.io = io;
+
+module.exports.userSocketMap = {};
+
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
+  console.log("User connected with id: ", userId);
+  if (userId) {
+    userSocketMap[userId] = socket.id;
+  }
+
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  socket.on("disconnect", () => {
+    console.log("User disconnected with id: ", userId);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
+
 app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
-app.use("api/v1/auth", userRouter)
+app.use("api/v1/auth", userRouter);
 
-connectDb()
+connectDb();
 
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 
 module.exports = httpServer;
