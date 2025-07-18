757e8373d93863883fdf6892d19a62ecb8743e16
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2025-07-18 21:18:24 +0500

    add socket connection on auth check to track online users

--- frontend/src/context/Auth/auth-provider.tsx
+++ frontend/src/context/Auth/auth-provider.tsx
@@ -7,9 +7,11 @@ import {
   type UpdateProfilePayload,
 } from "./auth-context";
 
-import { useState, type ReactNode } from "react";
+import { useCallback, useState, type ReactNode } from "react";
 import axios from "axios";
+import toast from "react-hot-toast";
 import { io, type Socket } from "socket.io-client";
+import type { AxiosError } from "axios";
 
 const backendUrl = import.meta.env.VITE_BACKEND_BASE_URL;
 axios.defaults.baseURL = backendUrl;
@@ -20,6 +22,44 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [onlineUsers, setOnlineUsers] = useState<string[]>([]);
   const [socket, setSocket] = useState<Socket | null>(null);
 
+  const connectedSocket = (userData: User) => {
+    if (!userData || socket?.connected) return;
+    const newSocket = io(backendUrl, {
+      query: { userId: userData._id },
+    });
+    newSocket.connect();
+    setSocket(newSocket);
+
+    newSocket.on("getOnlineUsers", (userIds: string[]) => {
+      setOnlineUsers(userIds);
+    });
+  };
+
+  const checkAuth = useCallback(async () => {
+    try {
+      const { data } = await axios.get("/auth/check");
+      if (data.success) {
+        setAuthUser(data.user);
+        connectedSocket(data.user);
+      }
+    } catch (error: unknown) {
+      console.error(error);
+
+      if (
+        typeof error === "object" &&
+        error !== null &&
+        "isAxiosError" in error
+      ) {
+        const axiosError = error as AxiosError<{ message: string }>;
+        const message = axiosError.response?.data?.message;
+        toast.error(message || "Request failed");
+      } else if (error instanceof Error) {
+        toast.error(error.message);
+      } else {
+        toast.error("Something went wrong");
+      }
+    }
+  }, []);
 
   const value: AuthContextType = {
     axios,
