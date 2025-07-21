3bacb55634f860a4dc40990e27073053ba3efef7
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2025-07-21 21:07:31 +0500

    protect routes based on auth state and add toast notifications

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,31 @@
-import { Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import Login from "./pages/Login"
-import Profile from "./pages/Profile"
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import { Toaster } from "react-hot-toast";
+import { useAuth } from "./context/Auth/useAuth";
 
 const App = () => {
+  const { authUser } = useAuth();
   return (
     <div className="bg-[url('./src/assets/bgImage.svg')] bg-cover bg-no-repeat">
+      <Toaster />
       <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/login" element={<Login/>} />
-      <Route path="/profile" element={<Profile/>} />
-    </Routes>
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <Login /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/profile"
+          element={authUser ? <Profile /> : <Navigate to="/login" />}
+        />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
