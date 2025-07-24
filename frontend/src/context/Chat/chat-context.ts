526801945954cd96fc05df2304b8134798c5cfbf
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2025-07-24 22:01:05 +0500

    Create chat context with types

---
+++ frontend/src/context/Chat/chat-context.ts
@@ -0,0 +1,40 @@
+import React, { createContext } from "react";
+
+export interface User {
+  _id: string;
+  fullName?: string;
+  email?: string;
+  bio?: string;
+  profileImage?: File | null | string;
+}
+
+
+export interface messagePayload {
+  text?: string;
+  image?: string;
+}
+
+export type Message = {
+  _id: string;
+  senderId: string;
+  receiverId: string;
+  text?: string;
+  image?: string;
+  seen?: boolean;
+};
+
+export interface ChatContextType {
+  messages: Message[];
+  users: User[];
+  unseenMessages: Record<string, number>;
+  setUnseenMessages: React.Dispatch<React.SetStateAction<Record<string, number>>>
+  selectedUser: User | null;
+  setSelectedUser: React.Dispatch<React.SetStateAction<User | null>>;
+  getUsers: () => Promise<void>;
+  getMessages: (id: string) => Promise<void>;
+  sendMessages: (data: messagePayload) => Promise<void>;
+  subscribeToMessage: () => Promise<void>;
+  unSubscribeFromMessage: () => void;
+}
+
+export const ChatContext = createContext<ChatContextType | null>(null);
